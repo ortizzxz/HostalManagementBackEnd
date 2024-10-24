ba9e8b4deee5ca8d59d9bc595fa27ed08083cdfb
jesusO <jesusdortiz18@gmail.com>
2024-10-24 16:49:05 +0200

    upload (uploading repo)
    This initial commit only contains Spring initializer and the structure of the folders of the backend - no code has been made

---
+++ src/main/java/com/hostalmanagement/app/HostalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hostalmanagement.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostalManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostalmanagement/app/HostalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostalmanagement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
