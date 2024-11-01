677c8fd41ced5cdb6ae97c2418339f87b22a8549
jesusO <jesusdortiz18@gmail.com>
2024-11-01 15:46:01 +0100

    ReservaHuesped DAO and its Impl developed
    quite unsure about the methods i implements so once the app is ready depending on user requiremetns im gonna modify the methods

---
+++ src/main/java/com/hostalmanagement/app/daoimpl/ReservaHuespedDAOImpl.java
@@ -0,0 +1,54 @@
+package com.hostalmanagement.app.daoimpl;
+
+import java.util.List;
+
+import com.hostalmanagement.app.dao.ReservaHuespedDAO;
+import com.hostalmanagement.app.model.ReservaHuesped;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+public class ReservaHuespedDAOImpl implements ReservaHuespedDAO{
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public ReservaHuesped findByNIF(String NIF) {
+        return entityManager.createQuery("FROM reservaHuesped r WHERE r.NIFHuesped like :NIF", ReservaHuesped.class)
+                .setParameter("NIF", NIF)
+                .getSingleResult();
+    }
+
+    @Override
+    public ReservaHuesped findByReserva(Long idReserva) {
+        return entityManager.createQuery("FROM reservaHuesped r WHERE r.NIFHuesped like :idReserva", ReservaHuesped.class)
+                .setParameter("idReserva", idReserva)
+                .getSingleResult();
+    }
+
+    @Override
+    public List<ReservaHuesped> findAll() {
+        return entityManager.createQuery("FROM reservaHuesped r", ReservaHuesped.class)
+                .getResultList();
+    }
+
+    @Override
+    public void save(ReservaHuesped reservaHuesped) {
+        entityManager.persist(reservaHuesped);
+    }
+
+    @Override
+    public void update(ReservaHuesped reservaHuesped) {
+        entityManager.merge(reservaHuesped);
+    }
+
+    @Override
+    public void removeByNIF(String NIF) {
+        ReservaHuesped reservaHuesped = findByNIF(NIF);
+        if(reservaHuesped != null){
+            entityManager.remove(reservaHuesped);
+        }
+    }
+    
+}
